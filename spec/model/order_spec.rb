b2d965fb5dddaf8f55ebcde9b7f4c29b1996d348
hantt-2856 <nguyen.thi.thanh.ha@sun-asterisk.com>
2022-01-24 10:52:10 +0700

    [Admin] test_orders_controler

--- spec/model/order_spec.rb
+++ spec/model/order_spec.rb
@@ -2,18 +2,17 @@ require "rails_helper"
 require "support/factory_bot"
 
 RSpec.describe Order, type: :model do
-  subject {create(:order)}
+  subject {create :order}
 
-  describe "be valid" do
-    it "has a valid factory" do
-      expect(subject).to be_valid
-    end
+  describe "be valid factory" do
+    it {is_expected.to be_valid}
   end
 
-  describe "enums" do
-    it "define status as an enum" do
-      should define_enum_for(:status)
-    end
+  describe "status as an enums" do
+    it {is_expected.to define_enum_for(:status)}
+    # it "define status as an enum" do
+    #   should define_enum_for(:status)
+    # end
   end
 
   describe "Validates" do
@@ -27,14 +26,15 @@ RSpec.describe Order, type: :model do
   end
 
   describe "Delegate" do
-    it {should delegate_method(:fullname).to(:user).with_prefix}
-    it {should delegate_method(:email).to(:user).with_prefix}
+    it {is_expected.to delegate_method(:fullname).to(:user).with_prefix}
+    it {is_expected.to delegate_method(:email).to(:user).with_prefix}
   end
 
   describe "#sort_orders" do
-    it "order by status asc and created_at desc" do
-      order_1 = create(:order, status: 3)
-      order_2 = create(:order, status: 0)
+    let(:order_1) {create(:order, status: 3)}
+    let(:order_2) {create(:order, status: 0)}
+    # it "order by status asc and created_at desc" do
+    it do
       expect(Order.sort_orders).to eq([order_2, order_1])
     end
   end
@@ -42,20 +42,23 @@ RSpec.describe Order, type: :model do
   describe "#update_quantity_product" do
     let(:order_detail_1) {create(:order_detail, order_id: subject.id)}
     let(:order_detail_2) {create(:order_detail, order_id: subject.id)}
-
-    it "increase product quantity if status change to rejected" do
-      expect(subject.update!(status: Order.statuses[:rejected])).to be true
-      subject.update_quantity_product
-      subject.order_details.each do |item|
-        expect(item.product.quantity).to eq(52)
+    context "when status change to rejected" do
+      it "increase product quantity" do
+        subject.update!(status: Order.statuses[:rejected])
+        subject.update_quantity_product
+        subject.order_details.each do |item|
+          expect(item.product.quantity).to eq(52)
+        end
       end
     end
 
-    it "NOT change product quantity if status NOT be rejected" do
-      expect(subject.update!(status: Order.statuses[:accept])).to be true
-      subject.update_quantity_product
-      subject.order_details.each do |item|
-        expect(item.product.quantity).to eq(50)
+    context "when status NOT change to rejected" do
+      it "NOT change product quantity" do
+        subject.update!(status: Order.statuses[:accept])
+        subject.update_quantity_product
+        subject.order_details.each do |item|
+          expect(item.product.quantity).to eq(50)
+        end
       end
     end
   end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,9 @@
-require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../config/environment', __dir__)
+require "spec_helper"
+ENV["RAILS_ENV"] ||= "test"
+require File.expand_path("../config/environment", __dir__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
-require 'rspec/rails'
+require "rspec/rails"
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 
 Dir[Rails.root.join("spec", "support", "**", "*.rb")].each { |f| require f }
 
@@ -20,7 +21,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
 end
 
-require 'shoulda/matchers'
+require "shoulda/matchers"
 Shoulda::Matchers.configure do |config|
   config.integrate do |with|
     with.test_framework :rspec
